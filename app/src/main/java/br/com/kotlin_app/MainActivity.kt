480244bf9684cc3aff6e76ed7609f9754fc1b068
Fernando Batista <nandosbx@gmail.com>
2020-09-03 19:48:58 -0300

    Hide Keyboard

--- app/src/main/java/br/com/kotlin_app/MainActivity.kt
+++ app/src/main/java/br/com/kotlin_app/MainActivity.kt
@@ -1,11 +1,15 @@
 package br.com.kotlin_app
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,36 +19,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val estadosArray = arrayListOf(
-            "Estados…",
-            "Acre",
-            "Alagoas",
-            "Amapá",
-            "Amazonas",
-            "Bahia",
-            "Ceará",
-            "Distrito Federal",
-            "Espírito Santo",
-            "Goiás",
-            "Maranhão",
-            "Mato Grosso",
-            "Mato Grosso do Sul",
-            "Minas Gerais",
-            "Pará",
-            "Paraíba",
-            "Paraná",
-            "Pernambuco",
-            "Piauí",
-            "Rio de Janeiro",
-            "Rio Grande do Norte",
-            "Rio Grande do Sul",
-            "Rondônia",
-            "Roraima",
-            "Santa Catarina",
-            "São Paulo",
-            "Sergipe",
-            "Tocantins"
-        )
+        val estadosArray = arrayListOf(R.array.states)
+//            "Estados…",
+//            "Acre",
+//            "Alagoas",
+//            "Amapá",
+//            "Amazonas",
+//            "Bahia",
+//            "Ceará",
+//            "Distrito Federal",
+//            "Espírito Santo",
+//            "Goiás",
+//            "Maranhão",
+//            "Mato Grosso",
+//            "Mato Grosso do Sul",
+//            "Minas Gerais",
+//            "Pará",
+//            "Paraíba",
+//            "Paraná",
+//            "Pernambuco",
+//            "Piauí",
+//            "Rio de Janeiro",
+//            "Rio Grande do Norte",
+//            "Rio Grande do Sul",
+//            "Rondônia",
+//            "Roraima",
+//            "Santa Catarina",
+//            "São Paulo",
+//            "Sergipe",
+//            "Tocantins"
+
 
         val spinner: Spinner = findViewById(R.id.spnMainEstados)
         // Create an ArrayAdapter using the string array and a default spinner layout
@@ -64,21 +68,71 @@ class MainActivity : AppCompatActivity() {
             val estados = spnMainEstados.selectedItem
             val valor = editTextMainValor.text.trim()
             val number: Double = valor.toString().toDouble()
-            val message = "O valor do IPVA a pagar é de: R$"
+            val message = "O valor do IPVA  no estado $estados para pagar é de: R$"
 
             //Validação dos campos
             if (valor.isEmpty() || estados == estadosArray[0]) {
 
                 //Apresentando um toast de erro ao usuário
                 Toast.makeText(this, "Preencha todos os campos", Toast.LENGTH_LONG).show()
-            } else if (estados == "Acre (AC)" || estados == "Tocantins (TO)") {
+
+            }
+            //TAX 2%
+            else if (estados == "Acre (AC)" || estados == "Tocantins (TO)" || estados == "Santa Catarina (SC)"
+                || estados == "Sergipe (SE)" || estados == "Paraíba (PB)" ) {
 
                 val tax = 0.02
                 val ipva: Double = number * tax
 
-                textResult.text = "$message $ipva"
-            }
+                textResult.text = "$message $ipva, o valor da taxa é de $tax."
 
+            }
+            //TAX 2,5%
+            else if (estados == "Alagoas (AL)" || estados == "Pernambuco (PE)"
+                || estados == "Rio Grande do Norte (RN)" || estados == "Ceará (CE)"
+                ||  estados == "Piauí (PI)" || estados == "Maranhão (MA)"
+                || estados == "Bahia (BA)" || estados == "Pará (PA)"
+                || estados == "Mato Grosso do Sul (MS)" || estados == "Goiás (GO)") {
+
+                val tax = 0.025
+                val ipva: Double = number * tax
+
+                textResult.text = "$message $ipva, o valor da taxa é de $tax."
+
+            }
+            //TAX 3%
+            else if (estados == "Rio Grande do Sul (RS)" || estados == "São Paulo (SP)"
+                || estados == "Amazonas (AM)" || estados == "Amapá (AP)"
+                || estados == "Roraima (RR)" || estados == "Rondônia (RO)"
+                || estados == "Mato Grosso (MT)" || estados == "Distrito Federal (DF)") {
+
+                val tax = 0.03
+                val ipva: Double = number * tax
+
+                textResult.text = "$message $ipva, o valor da taxa é de $tax."
+
+            }
+            //TAX 3.5%
+            else if (estados == "Paraná (PR)") {
+
+                val tax = 0.035
+                val ipva: Double = number * tax
+
+                textResult.text = "$message $ipva, o valor da taxa é de $tax."
+
+            }
+            //TAX 4%
+            else if (estados == "Rio de Janeiro (RJ)" || estados == "Minas Gerais (MG)") {
+
+                val tax = 0.04
+                val ipva: Double = number * tax
+
+                textResult.text = "$message $ipva, o valor da taxa é de $tax."
+
+            }
+            //Esconder o soft keyboard quando o botão for clicado
+            val inputManager: InputMethodManager =getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputManager.hideSoftInputFromWindow(currentFocus?.windowToken, InputMethodManager.SHOW_FORCED)
         }
     }
 }
